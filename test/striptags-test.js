88848d6353f77e7bc90cd222295db27f2af6d41a
Eric Norris <erictnorris@gmail.com>
2015-04-22 00:06:31 -0400

    Use correct ordering of actual vs. expected parameters to assert.

--- test/striptags-test.js
+++ test/striptags-test.js
@@ -8,28 +8,28 @@ describe('striptags', function() {
     it('should not modify plain text', function() {
         var text = 'lorem ipsum < a>';
 
-        assert.equal(text, striptags(text));
+        assert.equal(striptags(text), text);
     });
 
     it('should remove simple HTML tags', function() {
         var html = '<a href="">lorem <strong>ipsum</strong></a>',
             text = 'lorem ipsum';
 
-        assert.equal(text, striptags(html));
+        assert.equal(striptags(html), text);
     });
 
     it('should leave HTML tags if specified', function() {
         var html = '<strong>lorem ipsum</strong>',
             allowedTags = '<strong>';
 
-        assert.equal(html, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), html);
     });
 
     it('should leave attributes when allowing HTML', function() {
         var html = '<a href="https://example.com">lorem ipsum</a>',
             allowedTags = '<a>';
 
-        assert.equal(html, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), html);
     });
 
     it('should leave nested HTML tags if specified', function() {
@@ -37,7 +37,7 @@ describe('striptags', function() {
             strippedHtml = 'lorem <strong>ipsum</strong>',
             allowedTags = '<strong>';
 
-        assert.equal(strippedHtml, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), strippedHtml);
     });
 
     it('should leave outer HTML tags if specified', function() {
@@ -45,21 +45,21 @@ describe('striptags', function() {
             strippedHtml = '<div>lorem ipsum</div>',
             allowedTags = '<div>';
 
-        assert.equal(strippedHtml, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), strippedHtml);
     });
 
     it('should remove DOCTYPE declaration', function() {
         var html = '<!DOCTYPE html> lorem ipsum',
             text = ' lorem ipsum';
 
-        assert.equal(text, striptags(html));
+        assert.equal(striptags(html), text);
     });
 
     it('should remove comments', function() {
         var html = '<!-- lorem ipsum --> dolor sit amet',
             text = ' dolor sit amet';
 
-        assert.equal(text, striptags(html));
+        assert.equal(striptags(html), text);
     });
 
     it('should strip <> within quotes', function() {
@@ -67,7 +67,7 @@ describe('striptags', function() {
             strippedHtml = '<a href="script">lorem ipsum</a>',
             allowedTags = '<a>';
 
-        assert.equal(strippedHtml, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), strippedHtml);
     });
 
     it('should strip extra < within tags', function() {
@@ -75,32 +75,32 @@ describe('striptags', function() {
             strippedHtml = '<div>lorem ipsum</div>',
             allowedTags = '<div>';
 
-        assert.equal(strippedHtml, striptags(html, allowedTags));
+        assert.equal(striptags(html, allowedTags), strippedHtml);
     });
 
     it('should strip tags within comments', function() {
         var html = '<!-- <strong>lorem ipsum</strong> --> dolor sit',
             text = ' dolor sit';
 
-        assert.equal(text, striptags(html));
+        assert.equal(striptags(html), text);
     });
 
     it('should strip comment-like tags', function() {
         var html = '<! lorem ipsum> dolor sit',
             text = ' dolor sit';
 
-        assert.equal(text, striptags(html));
+        assert.equal(striptags(html), text);
     });
 
     it('should leave normal exclamation points alone', function() {
         var text = 'lorem ipsum! dolor sit amet';
 
-        assert.equal(text, striptags(text));
+        assert.equal(striptags(text), text);
     });
 
     it('should allow an array parameter for allowable tags', function() {
-        var html = '<strong>lorem ipsum</strong>',
-            allowedTags = ['strong'];
+        var html = '<strong>lorem <em>ipsum</em></strong>',
+            allowedTags = ['strong', 'em'];
 
         assert.equal(striptags(html, allowedTags), html);
     });
