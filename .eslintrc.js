6d6b101a85d786d80ecf90ba9ae358d571e3a895
cesine <cesine@yahoo.com>
2020-10-08 17:26:13 -0400

    eslint  --init

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "rules": {
+    }
+};
