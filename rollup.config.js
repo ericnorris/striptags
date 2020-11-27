487aa0105bbc37f9f29e05a51530c112cd38ab6d
Eric Norris <erictnorris@gmail.com>
2020-11-27 14:06:15 -0500

    ci: add github workflow

--- rollup.config.js
+++ rollup.config.js
@@ -3,18 +3,22 @@ import typescript from "@rollup/plugin-typescript";
 export default [
     {
         input: "src/striptags.ts",
-        output: [{
-            format: "es",
-            dir: "lib/",
-        }],
-        plugins: [typescript({declaration: true, declarationDir: "lib", outDir: "lib"})],
+        output: [
+            {
+                format: "es",
+                dir: "lib/",
+            },
+        ],
+        plugins: [typescript({ declaration: true, declarationDir: "lib", outDir: "lib" })],
     },
     {
         input: "src/striptags.ts",
-        output: [{
-            format: "cjs",
-            file: "bundle/striptags.js",
-        }],
+        output: [
+            {
+                format: "cjs",
+                file: "bundle/striptags.js",
+            },
+        ],
         plugins: [typescript()],
     },
 ];
