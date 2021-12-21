30fe13717f070ef9980d24ffa24d7844b18db0a8
Sergey Todyshev <stodyshev@gmail.com>
2021-12-22 00:04:51 +0300

    test: add failing test to handle case with malformed html

--- tests/striptags.test.ts
+++ tests/striptags.test.ts
@@ -1,4 +1,5 @@
 import { StateMachineOptions, StateMachine, striptags } from "../src/striptags";
+import { Parser } from "htmlparser2";
 
 const OptionsWithAllowedTags: Partial<StateMachineOptions> = {
     allowedTags: new Set(["atag"]),
@@ -66,4 +67,56 @@ describe("striptags", () => {
 
         expect(got).toEqual(WantWhenUsingDisallowedTags);
     });
+
+    it("can handle bad html", () => {
+        const badHtml = `
+<p><mark class="green">{{first_name}}</mark>,</p>
+
+Your recent LinkedIn post on <mark class="green">{{! POST TOPIC }}</mark> made me want to
+reach out. I’d love to know how you’re addressing
+<span class="painpoint contenteditable="false"
+    >Generating more leads that fit your ICP.</span
+>
+
+and what your thoughts are on
+<mark class="blue"
+    >Use more relevant account-level insights to know which companies that fit your ICP are ready to
+    buy now, not later.</mark
+>. If you’re interested in talking more, I’m free
+<span class="timeslot" contenteditable="false">Tuesday @ 11:00am</span>
+and
+<span class="timeslot" contenteditable="false">Thursday @ 1:00pm</span>
+for a quick call.
+<p></p>
+<p>Thanks,</p>
+<p><mark class="green">{{sender.first_name}}</mark></p>
+`;
+        const got = striptags(badHtml, {
+            tagReplacementText: "",
+        });
+        const better = stripHtml(badHtml);
+        expect(got.trim()).toEqual(better);
+    });
 });
+
+function stripHtml(html: string) {
+    html = (html || "").trim();
+    if (!html) {
+        return html;
+    }
+    const content: string[] = [];
+
+    const parser = new Parser(
+        {
+            ontext: (text) => {
+                content.push(text);
+            },
+        },
+        { decodeEntities: true },
+    );
+
+    parser.write(html);
+    parser.end();
+
+    return content.join("");
+}
